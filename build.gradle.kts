c4b461deda3ba261512c20fbd33cafadd71731ca
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2022-03-25 23:36:39 +0300

    update build.gradle.kts & gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,35 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    id("org.jetbrains.intellij") version "0.4.22"
-    java
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.serialization") version "1.3.72"
+    id("java")
+    id("org.jetbrains.intellij") version "1.4.0"
+
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+
     id("com.github.johnrengelman.shadow") version "5.1.0"
-    id("org.openjfx.javafxplugin") version "0.0.8"
-    id("com.gluonhq.client-gradle-plugin") version "0.0.11"
-    id("org.jetbrains.dokka") version "0.10.1"
+    id("org.openjfx.javafxplugin") version "0.0.12"
+    id("com.gluonhq.client-gradle-plugin") version "0.1.42"
+    id("org.jetbrains.dokka") version "1.6.10"
 }
 
-group = "org.jetbrains.research.ml.tasktracker"
-version = "1.1"
+group = properties("pluginGroup")
+version = properties("pluginVersion")
+
+intellij {
+    println("Using ide version: ${properties("platformVersion")}")
+    version.set(properties("platformVersion"))
+    pluginName.set(properties("pluginName"))
+}
 
 repositories {
-    maven (url = "https://www.jetbrains.com/intellij-repository/releases")
-    maven (url = "https://jetbrains.bintray.com/intellij-third-party-dependencies")
-    maven (url = "https://nexus.gluonhq.com/nexus/content/repositories/releases/")
-    maven (url = "https://jitpack.io")
+    maven(url = "https://www.jetbrains.com/intellij-repository/releases")
+    maven(url = "https://jetbrains.bintray.com/intellij-third-party-dependencies")
+    maven(url = "https://nexus.gluonhq.com/nexus/content/repositories/releases/")
+    maven(url = "https://jitpack.io")
 
     mavenCentral()
     jcenter()
@@ -30,24 +39,23 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.72")
-    implementation("com.opencsv","opencsv", "5.0")
-    implementation("joda-time", "joda-time", "2.9.2")
-    implementation("org.apache.commons", "commons-csv", "1.7")
+    //https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/migration.md
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("com.opencsv", "opencsv", "5.6")
+    implementation("joda-time", "joda-time", "2.10.14")
+    implementation("org.apache.commons", "commons-csv", "1.9.0")
     // https://mvnrepository.com/artifact/com.gluonhq/charm-glisten
-    implementation("com.gluonhq", "charm-glisten", "6.0.1")
-    implementation("com.google.code.gson", "gson", "2.8.5")
-    implementation("com.squareup.okhttp3", "okhttp", "4.2.2")
+    implementation("com.gluonhq", "charm-glisten", "6.1.0")
+    implementation("com.google.code.gson", "gson", "2.9.0")
+    implementation("com.squareup.okhttp3", "okhttp", "5.0.0-alpha.6")
 //    implementation("org.controlsfx:controlsfx:11.0.3")
-    compile("com.google.auto.service:auto-service:1.0-rc7")
+    implementation("com.google.auto.service:auto-service:1.0.1")
     implementation("org.eclipse.mylyn.github", "org.eclipse.egit.github.core", "2.1.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.9.1")
     // https://mvnrepository.com/artifact/net.lingala.zip4j/zip4j (used for unzipping required plugins)
-    implementation("net.lingala.zip4j", "zip4j", "2.6.1")
-    implementation("com.github.holgerbrandl:krangl:v0.13")
+    implementation("net.lingala.zip4j", "zip4j", "2.9.1")
+    implementation("com.github.holgerbrandl:krangl:0.17.3")
 
-    testCompile("junit", "junit", "4.12")
+    testImplementation("junit", "junit", "4.12")
 }
 
 /*
@@ -66,10 +74,12 @@ dependencies {
 
 /*
    Uncomment for testing with Intellij IDEA
-*/
+   Configure gradle-intellij-plugin plugin.
+   Read more: https://github.com/JetBrains/gradle-intellij-plugin
+
 intellij {
-    version = "2020.3"
-}
+    version.set(properties("platformVersion"))
+}*/
 
 
 /*
@@ -80,20 +90,6 @@ intellij {
 //    type = "PY"
 //}
 
-
-intellij {
-    val ideVersion = System.getenv().getOrDefault(
-        "CODE_TRACKER_IDEA_VERSION",
-        "201.6668.113"
-    )
-    println("Using ide version: $ideVersion")
-    version = ideVersion
-    pluginName = "code-tracker-plugin"
-    downloadSources = true
-    sameSinceUntilBuild = false
-    updateSinceUntilBuild = false
-}
-
 javafx {
     version = "15"
     modules("javafx.controls", "javafx.fxml", "javafx.swing")
@@ -101,28 +97,34 @@ javafx {
 }
 
 configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-    changeNotes("""
+    changeNotes.set(
+        """
       Add change notes here.<br>
-      <em>most HTML tags may be used</em>""")
+      <em>most HTML tags may be used</em>"""
+    )
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = properties("javaVersion")
 }
 
 gluonClient {
-    reflectionList = arrayListOf("javafx.fxml.FXMLLoader", "com.gluon.hello.views.HelloPresenter",
-        "javafx.scene.control.Button", "javafx.scene.control.Label")
+    reflectionList = arrayListOf(
+        "javafx.fxml.FXMLLoader", "com.gluon.hello.views.HelloPresenter",
+        "javafx.scene.control.Button", "javafx.scene.control.Label"
+    )
 }
 tasks.withType<ShadowJar> {
-    project.logger.warn("Don't forget to:\n" +
-            "- set your remote server as baseUrl in QueryExecutor class\n" +
-            "- turn OFF org.jetbrains.research.ml.tasktracker.Plugin.testMode")
+    project.logger.warn(
+        "Don't forget to:\n" +
+                "- set your remote server as baseUrl in QueryExecutor class\n" +
+                "- turn OFF org.jetbrains.research.ml.tasktracker.Plugin.testMode"
+    )
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.8"
+    gradleVersion = properties("gradleVersion")
 }
