ebd4a5d4ed1a8f70ac317c87a863fa0c26747c45
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2022-04-27 02:05:49 +0300

    added NextTask button action

---
+++ src/main/kotlin/org/jetbrains/research/ml/tasktracker/services/NextButtonAction.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.research.ml.tasktracker.services
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import org.jetbrains.research.ml.tasktracker.ui.MainController
+
+class NextButtonAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        MainController.taskController.startSolvingNextTask()
+    }
+}
