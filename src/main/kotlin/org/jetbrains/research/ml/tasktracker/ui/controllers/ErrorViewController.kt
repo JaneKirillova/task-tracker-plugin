30ee14515f70131572306fcc756c005be03b5516
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-05-20 02:41:39 +0300

    deleted dependency on ViewControllerInterface.kt

--- src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/controllers/ErrorViewController.kt
+++ src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/controllers/ErrorViewController.kt
@@ -3,8 +3,8 @@ package org.jetbrains.research.ml.tasktracker.ui.controllers
 import com.intellij.ui.jcef.JBCefJSQuery
 import org.jetbrains.research.ml.tasktracker.ui.BrowserView
 
-class ErrorViewController : ViewControllerInterface {
-    override fun updateViewContent(view: BrowserView) {
+class ErrorViewController {
+    fun updateViewContent(view: BrowserView) {
         view.updateViewByUrl("http://tasktracker/ErrorPage.html")
     }
 
--- src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/controllers/ViewControllerInterface.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.research.ml.tasktracker.ui.controllers
-
-import org.jetbrains.research.ml.tasktracker.ui.BrowserView
-
-interface ViewControllerInterface {
-    fun updateViewContent(view: BrowserView)
-}
