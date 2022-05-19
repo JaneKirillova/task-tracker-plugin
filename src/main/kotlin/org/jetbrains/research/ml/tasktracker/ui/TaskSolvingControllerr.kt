05872828fc505777a977569f6a5cfc5e90c403fb
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-05-20 02:48:33 +0300

    updated TaskSolvingController. Added support for storing user properties, applying new properties and actions and turning back actions after the research

--- src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/TaskSolvingControllerr.kt
+++ src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/TaskSolvingControllerr.kt
@@ -1,45 +1,71 @@
 package org.jetbrains.research.ml.tasktracker.ui
 
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import org.jetbrains.research.ml.tasktracker.models.Task
-import org.jetbrains.research.ml.tasktracker.server.PluginServer
 import org.jetbrains.research.ml.tasktracker.tracking.TaskFileHandler
 import org.jetbrains.research.ml.tasktracker.ui.controllers.ViewState
-
 
 class TaskSolvingControllerr(private val taskIterator: Iterator<Task>) {
     private val logger: Logger = Logger.getInstance(javaClass)
 
+    private val storedIdeProperties = mutableMapOf<String, String>()
+    private val appliedActions = mutableMapOf<String, Int>()
     var currentTask: Task? = null
         private set
 
     fun startSolvingNextTask(project: Project) {
-        ApplicationManager.getApplication().invokeLater {
+        /*PluginServer.sendDataForTask(it, project)
+        TaskFileHandler.closeTaskFiles(it)*/
+
+        if (taskIterator.hasNext()) {
+            currentTask = taskIterator.next()
+            logger.info("Start solving $currentTask")
+
             currentTask?.let {
-                PluginServer.sendDataForTask(it, project)
-                //TaskFileHandler.closeTaskFiles(it)
-            } ?: run {
-                val action = ActionManager.getInstance().getAction("HideAllWindows")
-                ActionManager.getInstance().tryToExecute(action, null, null, null, true)
+                ApplicationManager.getApplication().invokeLater {
+                    TaskFileHandler.openTaskFiles(it)
+                }
+
+                it.ideSettings.actionsToToggle.forEach { action ->
+                    logger.info("Toggle $action")
+                    appliedActions[action] = appliedActions.getOrDefault(action, 0) + 1
+                    executeIdeAction(action)
+                }
+                for ((key, value) in it.ideSettings.parameters) {
+                    //Mongoose database does not allow to store keys with dots
+                    val correctKey = key.replace("___", ".")
+
+                    if (!storedIdeProperties.containsKey(correctKey)) {
+                        PropertiesComponent.getInstance().getValue(correctKey)?.let { userPropertiesValue ->
+                            storedIdeProperties[correctKey] = userPropertiesValue
+                        }
+                    }
+                    PropertiesComponent.getInstance().setValue(correctKey, value)
+                }
             }
 
-            if (taskIterator.hasNext()) {
-                currentTask = taskIterator.next()
-                logger.info("Start solving $currentTask")
-                currentTask?.let { TaskFileHandler.openTaskFiles(it) }
-            } else {
-                //TODO at last task
-                logger.info("Solution Completed. Start uploading solutions.")
-                val action = ActionManager.getInstance().getAction("HideAllWindows")
-                ActionManager.getInstance().tryToExecute(action, null, null, null, true)
+        } else {
+            //TODO at last task
+            logger.info("Solution Completed. Start uploading solutions.")
 
-                MainController.successViewController.currentState = ViewState.FEEDBACK
-                MainController.browserViews.forEach {
-                    MainController.successViewController.updateViewContent(it)
+            for ((key, value) in storedIdeProperties) {
+                PropertiesComponent.getInstance().setValue(key, value)
+            }
+
+            for ((action, usages) in appliedActions) {
+                if (usages % 2 == 1) {
+                    executeIdeAction(action)
                 }
+            }
+
+            executeIdeAction("HideAllWindows")
+            MainController.successViewController.currentState = ViewState.FEEDBACK
+            MainController.browserViews.forEach {
+                MainController.successViewController.updateViewContent(it)
             }
         }
     }
@@ -48,4 +74,10 @@ class TaskSolvingControllerr(private val taskIterator: Iterator<Task>) {
 
     }
 
+    fun executeIdeAction(actionId: String) {
+        ApplicationManager.getApplication().invokeLater {
+            val action = ActionManager.getInstance().getAction(actionId)
+            ActionManager.getInstance().tryToExecute(action, null, null, null, true)
+        }
+    }
 }
