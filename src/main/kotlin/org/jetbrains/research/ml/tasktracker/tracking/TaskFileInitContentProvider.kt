ba65a960c941333d6372496d468797c207b47155
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-05-25 00:36:08 +0300

    added support for init jupyter notebook with content

--- src/main/kotlin/org/jetbrains/research/ml/tasktracker/tracking/TaskFileInitContentProvider.kt
+++ src/main/kotlin/org/jetbrains/research/ml/tasktracker/tracking/TaskFileInitContentProvider.kt
@@ -12,7 +12,7 @@ object TaskFileInitContentProvider {
 
     private fun getTaskComment(task: Task, language: Language): String {
         val commonCommentPart = "Write code for the ${task.key} task in this file\n\n"
-        return when(language) {
+        return when (language) {
             Language.PYTHON -> "# $commonCommentPart"
             Language.CPP -> "// ${commonCommentPart}To run this file add it to MAKE or CMAKE file in your project\n\n"
             // Default case, because in most languages is used // in comments
@@ -22,7 +22,7 @@ object TaskFileInitContentProvider {
 
     private fun getPackage(language: Language): String {
         val currentPackage = "package ${PLUGIN_FOLDER}.${language.name.toLowerCase()}"
-        return when(language) {
+        return when (language) {
             Language.JAVA -> "$currentPackage;\n\n"
             Language.KOTLIN -> "$currentPackage\n\n"
             else -> ""
@@ -34,8 +34,8 @@ object TaskFileInitContentProvider {
     }
 
     fun getTaskFileName(task: Task, language: Language): String {
-        return when(language) {
-            Language.JAVA  -> "${getClassNameByTask(task)}${language.extension.ext}"
+        return when (language) {
+            Language.JAVA -> "${getClassNameByTask(task)}${language.extension.ext}"
             Language.KOTLIN -> "${task.key.capitalize()}${language.extension.ext}"
             else -> "${task.key}${language.extension.ext}"
         }
@@ -43,7 +43,12 @@ object TaskFileInitContentProvider {
 
     fun getInitFileContent(task: Task, language: Language): String {
         val comment = getTaskComment(task, language)
-        return when(language) {
+        return when (language) {
+            //TODO: print condition at not only jupyter
+            Language.JUPYTER -> {
+                //Mongoose database does not allow to store keys with dots
+                task.description.toString().replace("___",".")
+            }
             Language.JAVA -> comment + "public class ${getClassNameByTask(task)} {\n" +
                     "    public static void main(String[] args) {\n" +
                     "        // Write your code here\n" +
