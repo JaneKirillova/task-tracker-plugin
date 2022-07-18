9c5a1827c65c23568d590c30200dbd1d3e22c5aa
Vseslav Kasatsky <71323710+samorojy@users.noreply.github.com>
2022-07-19 02:00:17 +0300

    updated UI (added darcula.css)

--- src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/util/SurveyHandler.kt
+++ src/main/kotlin/org/jetbrains/research/ml/tasktracker/ui/util/SurveyHandler.kt
@@ -16,6 +16,6 @@ fun getSurveyFactors(answers: List<Int>): String {
     if (factors.isEmpty()) {
         return "You have no attention difficulties that affect your everyday life."
     }
-    return "We found out that you may have some difficulties with" + factors.joinToString(", ") +
+    return "We found out that you may have some difficulties with " + factors.joinToString(", ") +
             ". If you feel that these issues affect your everyday life or you would like to know more about attention management, it might be helpful to discuss this with a psychologist of your choice."
 }
